86ded25051876d6dfdb40ae5060532139c50cf9b
NatanDuarte <natanprocopio@gmail.com>
2022-04-23 19:28:57 -0300

    feat(GlobalStyle): global style applyed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import styled from 'styled-components';
-import media from './media';
 import Container from './components/container/';
 import LeadingContainer from './components/leading-container/';
+import { GlobalStyle } from './components/GlobalStyle';
 
 export default function App() {
     return (
+        <>
+        <GlobalStyle/>
         <Container>
             <LeadingContainer>This is a Leading text</LeadingContainer>
         </Container>
+        </>
     );
 }
---
+++ src/components/GlobalStyle.js
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        text-decoration: none;
+
+        font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+    }
+`;
