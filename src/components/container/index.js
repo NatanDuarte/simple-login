ac9e09bac3de0c67ec51cb30df421d30cb604191
NatanDuarte <natanprocopio@gmail.com>
2022-04-23 21:45:07 -0300

    fix: removing redundant code

--- src/components/container/index.js
+++ src/components/container/index.js
@@ -9,22 +9,6 @@ const Container = styled.div`
     background-color: #F4F4F4;
 
     flex-direction: column;
-    ${media.desktop`
-        height: 100vh;
-        width: 100%;
-    `};
-    ${media.tablet`
-        height: 100vh;
-        width: 100%;
-    `};
-    ${media.widescreen`
-        height: 100vh;
-        width: 100%;
-    `};
-    ${media.uhd`
-        height: 100vh;
-        width: 100%;
-    `};
 `;
 
 export default Container;
