11ee8110c94e03f6ca8b437e85ce92715bba1414
NatanDuarte <natanprocopio@gmail.com>
2022-04-23 23:23:24 -0300

    feat: creating app colors file

---
+++ src/components/UI/colors.js
@@ -0,0 +1,8 @@
+export const primaryColor = '#8353CD';
+export const secundaryColor = '#DB73CD';
+
+export const lightBackgroundColor = '#F4F4F4';
+export const textColorLight = '#3D3D3D';
+
+export const darkBackgroundColor = '#4D444D';
+export const textColorDark = '#F1F4FF';
