7c5a41cc13f92a86bc9240b4638155855d7928b3
NatanDuarte <natanprocopio@gmail.com>
2022-04-26 18:39:24 -0300

    feat: form container created

---
+++ src/components/form-container/index.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const FormCotainer = styled.div`
+    width: 60%;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`;
+
+export default FormCotainer;
